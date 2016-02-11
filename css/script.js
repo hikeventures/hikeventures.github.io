bea6758491b362cc1217a98a8c1c909a7fb9b6a9
HikeVentures <hikeventures@users.noreply.github.com>
2016-02-11 12:26:42 +0200

    Update script.js

--- css/script.js
+++ css/script.js
@@ -127,8 +127,8 @@ $.get("http://ipinfo.io", function(response) {
     if(response.country == "US") {
         $("#ad").html("US AD HTML");
     }
-    else if(response.country == "FI") {
-        $("#ad").html("FI AD HTML");
+     else if(response.country == "GB") {
+        $("#ad").html("GB AD HTML");
     }
     else {
         $("#ad").html("DEFAULT HTML");
