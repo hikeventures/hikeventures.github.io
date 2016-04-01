10b1e82554ba7edcbd28e5f9d5ccfcf042b1d114
HikeVentures <hikeventures@users.noreply.github.com>
2016-04-01 14:40:17 +0300

    Update array.js

--- css/array.js
+++ css/array.js
@@ -131,7 +131,7 @@
     			var i;
 			var out = "<table>";
 
-    			for(i = 0; i < 6; i++) {
+    			for(i = 0; i < 5; i++) {
         			out += "<tr><td>" +
         			productArray[0][i].strProductName +
         			"</td><td>" +
