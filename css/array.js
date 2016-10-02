61aa079096965700be3a0216e631c830d8a77d95
HikeVentures <hikeventures@users.noreply.github.com>
2016-10-02 14:27:20 +0300

    Update array.js

--- css/array.js
+++ css/array.js
@@ -1,13 +1,3 @@
-var existingTitle = 'Sale Price';
-var newTitle = 'Price';
-
-var tableHeaderElements = document.getElementsByTagName('th');
-for (var i = 0; i < tableHeaderElements.length; i++) {
-    if (tableHeaderElements[i].innerHTML.indexOf(searchString) !== -1) {
-        tableHeaderElements[i].innerHTML=tableHeaderElements[i].innerHTML.replace(existingTitle,newTitle);          break;
-        
-    }
-}
 
 
 // HTML Display Portion  //
