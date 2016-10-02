5b4bbedc8c25cabaf3a41d07d303b918a52f7e46
HikeVentures <hikeventures@users.noreply.github.com>
2016-10-02 14:29:44 +0300

    Update script.js

--- css/script.js
+++ css/script.js
@@ -1,13 +1,4 @@
-var existingTitle = 'Sale Price';
-var newTitle = 'Price';
 
-var tableHeaderElements = document.getElementsByTagName('th');
-for (var i = 0; i < tableHeaderElements.length; i++) {
-    if (tableHeaderElements[i].innerHTML.indexOf(searchString) !== -1) {
-        tableHeaderElements[i].innerHTML=tableHeaderElements[i].innerHTML.replace(existingTitle,newTitle);          break;
-        
-    }
-}
 
 
 
